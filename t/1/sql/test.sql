018e9a90e5f8507fd97b2ea5b2c3201855da8f51
Satoshi Nagayasu <satoshi.nagayasu@gmail.com>
2012-07-16 19:18:52 +0900

    - Added boolean conditional tests to the unit test.

--- t/1/sql/test.sql
+++ t/1/sql/test.sql
@@ -4,3 +4,25 @@ SELECT id,title FROM dc_table_1 WHERE content @@ 'Brazilian';
 SELECT id,title FROM dc_table_1 WHERE content @@ 'brazilian';
 -- `brazil' is a different word from `Brazilian'.
 SELECT id,title FROM dc_table_1 WHERE content @@ 'brazil';
+
+-- `Cocoa'.
+SELECT id,title FROM dc_table_1 WHERE content @@ 'Cocoa';
+
+-- `Chocolate' - Must fail.
+SELECT id,title FROM dc_table_1 WHERE content @@ 'Chocolate';
+
+--
+-- BOOLEAN conditional test
+--
+
+-- `Brazillian' AND 'Cocoa'.
+SELECT id,title FROM dc_table_1 WHERE content @@ 'Brazilian' AND content @@ 'Cocoa';
+
+-- `Brazillian' AND 'Chocolate' - Must fail.
+SELECT id,title FROM dc_table_1 WHERE content @@ 'Brazilian' AND content @@ 'Chocolate';
+
+-- `Brazillian' OR 'Cocoa'.
+SELECT id,title FROM dc_table_1 WHERE content @@ 'Brazilian' OR content @@ 'Cocoa';
+
+-- `Brazillian' OR 'Chocolate'.
+SELECT id,title FROM dc_table_1 WHERE content @@ 'Brazilian' OR content @@ 'Chocolate';
